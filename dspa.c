a8829342cd699ad928bdaf99eb222c3150602d99
JanFan <termal12345@gmail.com>
2018-03-28 12:07:56 +0600

    Start

--- dspa.c
+++ dspa.c
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//
 #include <linux/cdev.h>
 #include <linux/kernel.h>
 #include <linux/miscdevice.h>
